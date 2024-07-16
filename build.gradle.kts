6b2898d4ba98955298713cc3a33a857449b1cf94
jaehyeong <wogud1514@gmail.com>
2024-07-16 17:37:11 +0900

    chore: spring-retry

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,10 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+	//retry
+	implementation("org.springframework.retry:spring-retry:2.0.3")
+
 	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
