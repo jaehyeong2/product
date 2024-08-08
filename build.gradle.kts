cf5eb2aeebaf92821638fca01ac8496466ebebb4
jaehyeong2 <wogud1514@gmail.com>
2024-08-08 14:29:10 +0900

    chore: spring kafka

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,7 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.kafka:spring-kafka")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 
 	//cache
